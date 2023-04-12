39a5e43781223cc37c1427e902d9cc4d54e1c236
Shamman626 <m15302938395@163.com>
2023-04-12 22:17:25 +0800

    23/04/12 后端Controller相关（R文件，前端get请求时使用）

--- src/main/java/com/vo/R.java
+++ src/main/java/com/vo/R.java
@@ -1,12 +1,63 @@
 package com.vo;
 
+import lombok.Data;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * FileName: R
- * Date: 2023/04/02
- * Description: 该类用于传输数据
+ * Date: 2023/04/12
+ * Description: 该类用于传输数据，为Response文件，返回值时使用
  */
-
+@Data
 public class R {
+
+    //是否成功
+    private Boolean success;
+    //返回消息
+    private String message;
+
+    //返回数据
+    private Map<String, Object> data = new HashMap<>();
+
+    // 把构造方法私有化
+    private R() {}
+
+    // 成功静态方法
+    public static R ok() {
+        R r = new R();
+        r.setSuccess(true);
+        r.setMessage("成功");
+        return r;
+    }
+
+    // 失败静态方法
+    public static R error() {
+        R r = new R();
+        r.setSuccess(false);
+        r.setMessage("失败");
+        return r;
+    }
+
+    // 消息
+    public R message(String message){
+        this.setMessage(message);
+        return this;
+    }
+
+    // 数据
+    public R data(String key, Object value){
+        this.data.put(key, value);
+        return this;
+    }
+    //数据
+    public R data(Map<String, Object> map){
+        this.setData(map);
+        return this;
+    }
+
+
 
 
 
