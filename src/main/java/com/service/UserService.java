8beab32615d5b2adfbfa85928d90875492e874a0
xixilili423 <1782532694@qq.com>
2023-04-24 13:32:46 +0800

    修改密码/修改个人信息

--- src/main/java/com/service/UserService.java
+++ src/main/java/com/service/UserService.java
@@ -53,18 +53,6 @@ public interface UserService extends IService<User> {
     R changeInformation(ChangeInfoParam changeInfoParam);
 
 
-    /**
-     * 4.修改密码
-     * @post changePassword()
-     * @return
-     */
-    R changePassword(ChangePasswordParam changePasswordParam);
 
-    /**
-     * 4.修改个人信息
-     * @post changeInfo()
-     * @return
-     */
-    R changeInfo(ChangeInfoParam changeInfoParam);
 
 }
